f7f1d0273a2066ccc92bb0a79dd3ac2c1ec8cab1
Lmaxplay <lucamollema@outlook.com>
2022-01-08 15:18:57 +0100

    Update

--- LMPTSBase/app.js
+++ LMPTSBase/app.js
@@ -6,20 +6,7 @@ const nodevm = new VM.NodeVM({});
 const colors = new chalk.Chalk();
 const logging = new Logger(process.stdout, process.stdin);
 // Start of actual code
-vm.setGlobal("logging", logging);
-vm.run(`logging.log("Hello World")`);
-try {
-    nodevm.run(`
-const fs = require("fs");
-
-console.log(fs.readdirSync("C:"));
-`);
-}
-catch (err) {
-    if (logging instanceof String) {
-        logging.log(err);
-    }
-}
+nodevm.runFile("./unsafe.js");
 // End Application
 process.exit(0);
 //# sourceMappingURL=app.js.map
--- LMPTSBase/app.ts
+++ LMPTSBase/app.ts
@@ -9,34 +9,14 @@ import * as readlineModule from 'node:readline';
 import * as marked from 'marked';
 import * as Seven from 'node-7z';
 
-import * as VM from 'vm2';
-
 import Logger from './Modules/logger.js';
 import { LogLevel } from './Modules/logger.js';
-
-const vm = new VM.VM({"compiler": "javascript"});
-const nodevm = new VM.NodeVM({});
 
 const colors = new chalk.Chalk();
 
 const logging = new Logger(process.stdout, process.stdin);
 
 // Start of actual code
-
-vm.setGlobal("logging", logging);
-
-vm.run(`logging.log("Hello World")`);
-try {
-nodevm.run(`
-const fs = require("fs");
-
-console.log(fs.readdirSync("C:"));
-`);
-} catch(err: any) {
-    if(logging instanceof String) {
-        logging.log(err);
-    }
-}
 
 // End Application
 
