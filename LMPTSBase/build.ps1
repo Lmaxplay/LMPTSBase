b925dcc6d8dd2c138f44f8472284ce7108fe476a
Lmaxplay <lucamollema@outlook.com>
2022-01-08 14:01:01 +0100

    Updated build script :D

--- LMPTSBase/build.ps1
+++ LMPTSBase/build.ps1
@@ -1,23 +1,37 @@
 param(
-    [Parameter(Mandatory=$False)][System.Boolean][System.Int32]$tsskip=0,
-    [Parameter(Mandatory=$False)][System.String]$file="",
-    [Parameter(Mandatory=$False)][System.String]$tsconfig=""
+    [Parameter(Mandatory=$False)][System.Boolean][System.Int32]$tsskip=0, # skips typescript compilation if set to anything but 0
+    [Parameter(Mandatory=$False)][System.String]$file="", # file to run with node
+    [Parameter(Mandatory=$False)][System.String]$tsconfig="", # tsconfig.json location
+    [Parameter(Mandatory=$False)][System.Boolean][System.Int32]$strict=0 # enable --strict compiler option?
 )
-Write-Output 'Lmaxplay TS-Run v1.0, all rights reserved, Licensed under the MIT License'
+
+Write-Output 'Lmaxplay TS-Node build script v1.0, all rights reserved, Licensed under the MIT License' # TODO Update this bit to be more informative
+
+# TypeScript compilation
 if ($tsskip) {
-    Write-Output 'Skipping TypeScript compile due to arguments'
+    Write-Output 'Skipping TypeScript compiler due to arguments'
 } else {
-    Write-Output 'Running TypeScript compile'
+    Write-Output 'Running TypeScript compiler tsc'
     if ($tsconfig -eq "") {
         $tsconfig = "tsconfig.json"
     }
-    tsc -p "$tsconfig" # Run TypeScript compile
+    if($strict) {
+        $strictstring = '--strict';
+    } else {
+        $strictstring = '';
+    }
+
+    tsc -p "$tsconfig" $strictstring --allowJs # Run TypeScript compile
     Write-Output 'TypeScript compile complete'
 }
-Write-Output 'Running node application'
+
+# Setting up the filepath
 if($file -eq "") {
-    node app.js #Run app.js
+    $appstring = 'app.js';
 } else {
-    Write-Output "Running file $($file) using NodeJS"
-    node "$($args[0])" #Run first argument
+    $appstring = $file
 }
+
+# Actually running NodeJS
+Write-Output "Running node application using js file $file";
+node "$appstring"
